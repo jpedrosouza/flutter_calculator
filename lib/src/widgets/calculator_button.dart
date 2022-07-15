806aad99f3a14dce2e61c2eef9fe93dac366eff3
João Pedro Souza <jsouza.1650@gmail.com>
2022-07-14 23:16:48 -0300

    created CalculatorButton widget

---
+++ lib/src/widgets/calculator_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class CalculatorButton extends StatefulWidget {
+  final String text;
+  final Function(String) onPressed;
+  final bool isNumberValue;
+  final Color? backgroundColor;
+  final Color? textColor;
+
+  const CalculatorButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+    required this.isNumberValue,
+    this.backgroundColor,
+    this.textColor,
+  }) : super(key: key);
+
+  @override
+  State<CalculatorButton> createState() => _CalculatorButtonState();
+}
+
+class _CalculatorButtonState extends State<CalculatorButton> {
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () => widget.onPressed(widget.text),
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(
+          widget.backgroundColor ?? Colors.white,
+        ),
+        shape: MaterialStateProperty.all(
+          const RoundedRectangleBorder(
+            side: BorderSide(color: Colors.grey, width: 0.5),
+          ),
+        ),
+      ),
+      child: Text(
+        widget.text,
+        style: TextStyle(
+          color: widget.textColor ?? Colors.black,
+        ),
+      ),
+    );
+  }
+}
