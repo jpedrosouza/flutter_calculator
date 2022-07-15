0170f52a265130875f532b53de8c0341bf87517f
João Pedro Souza <jsouza.1650@gmail.com>
2022-07-14 23:43:04 -0300

    updated HomePage layout

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -17,8 +17,14 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Observer(
-        builder: (_) => SafeArea(
+        builder: (_) => Container(
+          color: Colors.grey[200],
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
             children: [
               Container(
                 width: MediaQuery.of(context).size.width,
@@ -53,181 +59,185 @@ class _HomePageState extends State<HomePage> {
   // Create grid 5x4 with calculator buttons
 
   Widget calculatorButtons() {
-    return GridView.count(
-      primary: false,
-      shrinkWrap: true,
-      crossAxisCount: 4,
-      children: [
-        CalculatorButton(
-          text: '%',
-          onPressed: (value) {
-            controller.setOperation(value);
-            controller.getValue();
-          },
-          isNumberValue: false,
-          textColor: const Color.fromARGB(255, 45, 120, 157),
-        ),
-        CalculatorButton(
-          text: '(',
-          onPressed: (value) {
-            controller.setDisplay(value);
-          },
-          isNumberValue: false,
-          textColor: const Color.fromARGB(255, 45, 120, 157),
-        ),
-        CalculatorButton(
-          text: ')',
-          onPressed: (value) {
-            controller.setDisplay(value);
-          },
-          isNumberValue: false,
-          textColor: const Color.fromARGB(255, 45, 120, 157),
-        ),
-        CalculatorButton(
-          text: '/',
-          onPressed: (value) {
-            controller.setOperation(value);
-            controller.getValue();
-          },
-          isNumberValue: false,
-          backgroundColor: const Color.fromARGB(255, 45, 120, 157),
-          textColor: Colors.white,
-        ),
-        CalculatorButton(
-          text: '7',
-          onPressed: (value) {
-            controller.setDisplay(value);
-          },
-          isNumberValue: true,
-          textColor: const Color.fromARGB(255, 45, 120, 157),
-        ),
-        CalculatorButton(
-          text: '8',
-          onPressed: (value) {
-            controller.setDisplay(value);
-          },
-          isNumberValue: true,
-          textColor: const Color.fromARGB(255, 45, 120, 157),
-        ),
-        CalculatorButton(
-          text: '9',
-          onPressed: (value) {
-            controller.setDisplay(value);
-          },
-          isNumberValue: true,
-          textColor: const Color.fromARGB(255, 45, 120, 157),
-        ),
-        CalculatorButton(
-          text: 'X',
-          onPressed: (value) {
-            controller.setOperation(value);
-            controller.getValue();
-          },
-          isNumberValue: false,
-          backgroundColor: const Color.fromARGB(255, 45, 120, 157),
-          textColor: Colors.white,
-        ),
-        CalculatorButton(
-          text: '4',
-          onPressed: (value) {
-            controller.setDisplay(value);
-          },
-          isNumberValue: true,
-          textColor: const Color.fromARGB(255, 45, 120, 157),
-        ),
-        CalculatorButton(
-          text: '5',
-          onPressed: (value) {
-            controller.setDisplay(value);
-          },
-          isNumberValue: true,
-          textColor: const Color.fromARGB(255, 45, 120, 157),
-        ),
-        CalculatorButton(
-          text: '6',
-          onPressed: (value) {
-            controller.setDisplay(value);
-          },
-          isNumberValue: true,
-          textColor: const Color.fromARGB(255, 45, 120, 157),
-        ),
-        CalculatorButton(
-          text: '-',
-          onPressed: (value) {
-            controller.setOperation(value);
-            controller.getValue();
-          },
-          isNumberValue: false,
-          backgroundColor: const Color.fromARGB(255, 45, 120, 157),
-          textColor: Colors.white,
-        ),
-        CalculatorButton(
-          text: '1',
-          onPressed: (value) {
-            controller.setDisplay(value);
-          },
-          isNumberValue: true,
-          textColor: const Color.fromARGB(255, 45, 120, 157),
-        ),
-        CalculatorButton(
-          text: '2',
-          onPressed: (value) {
-            controller.setDisplay(value);
-          },
-          isNumberValue: true,
-          textColor: const Color.fromARGB(255, 45, 120, 157),
-        ),
-        CalculatorButton(
-          text: '3',
-          onPressed: (value) {
-            controller.setDisplay(value);
-          },
-          isNumberValue: true,
-          textColor: const Color.fromARGB(255, 45, 120, 157),
-        ),
-        CalculatorButton(
-          text: '+',
-          onPressed: (value) {
-            controller.setOperation(value);
-            controller.getValue();
-          },
-          isNumberValue: false,
-          backgroundColor: const Color.fromARGB(255, 45, 120, 157),
-          textColor: Colors.white,
-        ),
-        CalculatorButton(
-          text: '0',
-          onPressed: (value) {
-            controller.setDisplay(value);
-          },
-          isNumberValue: true,
-          textColor: const Color.fromARGB(255, 45, 120, 157),
-        ),
-        CalculatorButton(
-          text: '.',
-          onPressed: (value) {
-            controller.setDisplay(value);
-          },
-          isNumberValue: false,
-        ),
-        CalculatorButton(
-          text: '<=',
-          onPressed: (value) {
-            controller.clearLast();
-          },
-          isNumberValue: false,
-          textColor: Colors.blueGrey,
-        ),
-        CalculatorButton(
-          text: '=',
-          onPressed: (value) {
-            controller.getValue();
-          },
-          isNumberValue: false,
-          backgroundColor: Colors.orange,
-          textColor: Colors.white,
-        ),
-      ],
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.7,
+      child: GridView.count(
+        crossAxisCount: 4,
+        childAspectRatio: (MediaQuery.of(context).size.width / 3.3) / ((MediaQuery.of(context).size.height * 0.57) / 3.6),
+        crossAxisSpacing: 2,
+        mainAxisSpacing: 2,
+        children: [
+          CalculatorButton(
+            text: '%',
+            onPressed: (value) {
+              controller.setOperation(value);
+              controller.getValue();
+            },
+            isNumberValue: false,
+            textColor: const Color.fromARGB(255, 45, 120, 157),
+          ),
+          CalculatorButton(
+            text: '(',
+            onPressed: (value) {
+              controller.setDisplay(value);
+            },
+            isNumberValue: false,
+            textColor: const Color.fromARGB(255, 45, 120, 157),
+          ),
+          CalculatorButton(
+            text: ')',
+            onPressed: (value) {
+              controller.setDisplay(value);
+            },
+            isNumberValue: false,
+            textColor: const Color.fromARGB(255, 45, 120, 157),
+          ),
+          CalculatorButton(
+            text: '/',
+            onPressed: (value) {
+              controller.setOperation(value);
+              controller.getValue();
+            },
+            isNumberValue: false,
+            backgroundColor: const Color.fromARGB(255, 45, 120, 157),
+            textColor: Colors.white,
+          ),
+          CalculatorButton(
+            text: '7',
+            onPressed: (value) {
+              controller.setDisplay(value);
+            },
+            isNumberValue: true,
+            textColor: const Color.fromARGB(255, 45, 120, 157),
+          ),
+          CalculatorButton(
+            text: '8',
+            onPressed: (value) {
+              controller.setDisplay(value);
+            },
+            isNumberValue: true,
+            textColor: const Color.fromARGB(255, 45, 120, 157),
+          ),
+          CalculatorButton(
+            text: '9',
+            onPressed: (value) {
+              controller.setDisplay(value);
+            },
+            isNumberValue: true,
+            textColor: const Color.fromARGB(255, 45, 120, 157),
+          ),
+          CalculatorButton(
+            text: 'X',
+            onPressed: (value) {
+              controller.setOperation(value);
+              controller.getValue();
+            },
+            isNumberValue: false,
+            backgroundColor: const Color.fromARGB(255, 45, 120, 157),
+            textColor: Colors.white,
+          ),
+          CalculatorButton(
+            text: '4',
+            onPressed: (value) {
+              controller.setDisplay(value);
+            },
+            isNumberValue: true,
+            textColor: const Color.fromARGB(255, 45, 120, 157),
+          ),
+          CalculatorButton(
+            text: '5',
+            onPressed: (value) {
+              controller.setDisplay(value);
+            },
+            isNumberValue: true,
+            textColor: const Color.fromARGB(255, 45, 120, 157),
+          ),
+          CalculatorButton(
+            text: '6',
+            onPressed: (value) {
+              controller.setDisplay(value);
+            },
+            isNumberValue: true,
+            textColor: const Color.fromARGB(255, 45, 120, 157),
+          ),
+          CalculatorButton(
+            text: '-',
+            onPressed: (value) {
+              controller.setOperation(value);
+              controller.getValue();
+            },
+            isNumberValue: false,
+            backgroundColor: const Color.fromARGB(255, 45, 120, 157),
+            textColor: Colors.white,
+          ),
+          CalculatorButton(
+            text: '1',
+            onPressed: (value) {
+              controller.setDisplay(value);
+            },
+            isNumberValue: true,
+            textColor: const Color.fromARGB(255, 45, 120, 157),
+          ),
+          CalculatorButton(
+            text: '2',
+            onPressed: (value) {
+              controller.setDisplay(value);
+            },
+            isNumberValue: true,
+            textColor: const Color.fromARGB(255, 45, 120, 157),
+          ),
+          CalculatorButton(
+            text: '3',
+            onPressed: (value) {
+              controller.setDisplay(value);
+            },
+            isNumberValue: true,
+            textColor: const Color.fromARGB(255, 45, 120, 157),
+          ),
+          CalculatorButton(
+            text: '+',
+            onPressed: (value) {
+              controller.setOperation(value);
+              controller.getValue();
+            },
+            isNumberValue: false,
+            backgroundColor: const Color.fromARGB(255, 45, 120, 157),
+            textColor: Colors.white,
+          ),
+          CalculatorButton(
+            text: '0',
+            onPressed: (value) {
+              controller.setDisplay(value);
+            },
+            isNumberValue: true,
+            textColor: const Color.fromARGB(255, 45, 120, 157),
+          ),
+          CalculatorButton(
+            text: '.',
+            onPressed: (value) {
+              controller.setDisplay(value);
+            },
+            isNumberValue: false,
+          ),
+          CalculatorButton(
+            text: '<=',
+            onPressed: (value) {
+              controller.clearLast();
+            },
+            isNumberValue: false,
+            textColor: Colors.blueGrey,
+          ),
+          CalculatorButton(
+            text: '=',
+            onPressed: (value) {
+              controller.getValue();
+            },
+            isNumberValue: false,
+            backgroundColor: Colors.orange,
+            textColor: Colors.white,
+          ),
+        ],
+      ),
     );
   }
 }
